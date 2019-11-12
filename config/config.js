2e5713d14145ac654c4494d598fdce75dcfb613e
ISRAEL JERONIMO DA SILVA <FR041710@catolica.ubec>
2019-11-11 21:52:53 -0300

    11/11/19

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "root",
     "database": "Picorestdb",
     "host": "localhost",
     "dialect": "mariadb",
--- controllers/funcionarioController.js
+++ controllers/funcionarioController.js
@@ -3,7 +3,7 @@ let db = require("../models")
 module.exports = {
 
     
-    findAll:async (req,res)=>{
+    /*findAll:async (req,res)=>{
         var funcionarioFiltrado = [];
             try{
                 let funcionario = await db.funcionario.findAll({
@@ -18,13 +18,14 @@ module.exports = {
                     },{
                         model: db.funcao,
                         attributes:['id','descricao']
-                     },{
-                        model: db.funcionariocurso,
-                        include: {
-                            model: db.curso,
-                            attributes:['id','descricao']
-                        }
-                     }
+                      }//,{
+                    //     model: db.funcionariocurso,
+                    //     attributes:['id','comprovante']
+                        // include: {
+                        //     model: db.curso,
+                        //     attributes:['id','descricao']
+                        // }
+                     //}
                     ]
                 })
                 for (var i = 0; i < funcionario.length; i++) {
@@ -33,8 +34,8 @@ module.exports = {
                  nome: funcionario[i].usuario.nome,
                  setor: funcionario[i].setor.descricao,
                  funcao: funcionario[i].funcao.descricao,
-                 //cursoId: funcionario[i].funcionarioCurso.cursoId,
-                 curso: funcionario[i].curso.descricao
+                 //cursoId: funcionario[i].funcionariocurso.comprovante
+                 //curso: funcionario[i].curso.descricao
                 });
     
                 }
@@ -44,20 +45,37 @@ module.exports = {
             catch(error){
                 res.sendStatus(400)
             }
-        },
-    
-    
-// findAll:async (req,res)=>{
-//     try{
-//         let usuario = await db.users.findAll({
-//             attributes:['id','name','email','age'],
-//             include: [{
-//                 model: db.tasks,
-//                 attributes:['id','title','descript','createdAt',"userId"]
-//             }]
-//         })
-//         res.json(users)
-//     }
+        },*/
+
+
+    findAll:async (req,res)=>{
+        try{
+            let funcionario = await db.funcionario.sequelize.query('SELECT u.nome, s.descricao AS setor, f.descricao AS funcao, fr.matricula, fc.comprovante, c.descricao AS curso FROM usuarios AS u INNER JOIN funcionarios AS fr ON u.id = fr.usuarioId INNER JOIN setors AS s ON s.id = fr.setorId INNER JOIN funcaos AS f ON f.id = fr.funcaoId INNER JOIN funcionarioCursos AS fc ON fr.id = fc.funcionarioId INNER JOIN cursos AS c ON c.id = fc.cursoId WHERE u.id = :userId ',
+            { replacements: { userId: req.body.usuarioId  }, type: db.funcionario.sequelize.QueryTypes.SELECT }
+            )
+            res.json(funcionario)
+        }
+        catch(error){
+            console.log(error)
+            res.sendStatus(400)
+        }
+    },
+
+
+
+//     sequelize.query('SELECT * FROM users WHERE name LIKE :search_name ',
+//     { replacements: { search_name: 'ben%'  }, type: sequelize.QueryTypes.SELECT }
+//   ).then(projects => {
+//     console.log(projects)
+//   }
+
+
+
+
+
+
+
+
     create: async(req,res)=>{
 
         try{
